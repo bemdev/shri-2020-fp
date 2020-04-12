01004ca15702aff62533a414e318de957cf4dd3b
bemdev <mediaforge1@yandex.ru>
2020-04-12 20:24:59 +0400

    SHRIHOMEWORKIII-666/well done | see fp#5 semi done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,101 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import * as R from 'ramda';
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
-
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
     writeLog(value);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+    const validateString = string => {
+        const len = string.length;
+        if (
+            len > 10 ||
+            len < 2 ||
+            parseInt(string, 0) < 0 ||
+            !string.match(/^[0-9]*[.,]?[0-9]+$/)
+        ) {
+            handleError('ValidationError');
+            return false;
+        } else {
+            return string;
+        }
+    };
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+    if (!validateString(value)) {
+        return;
+    }
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    const toNumberFix = value => {
+        const number = parseInt(value, 0);
+        writeLog(number);
+        return parseInt(value, 0);
+    };
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+    const number = toNumberFix(value);
+
+    const fetchNumber = number => {
+        return api
+            .get('https://api.tech/numbers/base', {
+                from: 10,
+                to: 2,
+                number: number,
+            })
+            .then(r => {
+                writeLog(r.result);
+                return r.result;
+            });
+    };
+
+    const fetchAnimal = id => {
+        return api.get(`https://animals.tech/`, { id: id }).then(r => r.result);
+    };
+
+    const getLenNumber = number => {
+        const len = String(number).length;
+        writeLog(len);
+        return parseInt(len, 0);
+    };
+    const makePow = number => {
+        const pow = Math.pow(number, 2);
+        writeLog(pow);
+        return pow;
+    };
+    const makeDivided = number => {
+        const divide = parseInt(number / 3, 0);
+        writeLog(divide);
+        return divide;
+    };
+
+    const makeWork = R.pipe(
+        fetchNumber,
+        R.andThen(
+            R.pipe(
+                getLenNumber,
+                makePow,
+                makeDivided,
+                fetchAnimal,
+                R.andThen(handleSuccess),
+            ),
+        ),
+    );
+
+    makeWork(number);
+};
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,6 @@
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -12,39 +12,139 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+import * as R from 'ramda';
+
+//mb this dont neeed but good fo me
+const objToArray = R.pipe(
+    R.toPairs,
+    R.map(
+        R.pipe(
+            R.of,
+            R.fromPairs,
+        ),
+    ),
+);
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = figures => {
+    const checkRule = R.allPass([
+        R.propEq('circle', 'white'),
+        R.propEq('triangle', 'white'),
+        R.propEq('star', 'red'),
+        R.propEq('square', 'green'),
+    ]);
 
-    return star === 'red' && square === 'green';
+    return checkRule(figures);
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = figures => {
+    const isGreen = fig => R.equals(fig, 'green');
+    const greenFigures = R.filter(isGreen, figures);
+
+    return R.length(objToArray(greenFigures)) > 1;
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = figures => {
+    const isRed = fig => R.equals(fig, 'red');
+    const isBlue = fig => R.equals(fig, 'blue');
+
+    const redFigures = R.filter(isRed, figures);
+    const blueFigures = R.filter(isBlue, figures);
+
+    return (
+        R.length(objToArray(redFigures)) === R.length(objToArray(blueFigures))
+    );
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = figures => {
+    const checkRule = R.allPass([
+        R.propEq('circle', 'blue'),
+        R.propEq('star', 'red'),
+        R.propEq('square', 'orange'),
+    ]);
+
+    return checkRule(figures);
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = figures => {
+    let referenceColor = figures.circle;
+
+    if (referenceColor === 'white') {
+        referenceColor = figures.square;
+    }
+
+    const isValid = R.allPass([
+        R.propEq('star', referenceColor),
+        R.propEq('triangle', referenceColor),
+        R.propEq('square', referenceColor),
+        R.propEq('circle', referenceColor),
+    ]);
+
+    return isValid(figures);
+};
 
 // 6. Две зеленые фигуры (одна из них треугольник), еще одна любая красная.
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = figures => {
+    const isGreen = fig => R.equals(fig, 'green');
+    const isRed = fig => R.equals(fig, 'red');
+
+    const greenFigures = R.filter(isGreen, figures);
+    const redFigures = R.filter(isRed, figures);
+
+    const hasTriagle = R.has('triangle');
+
+    if (hasTriagle(greenFigures)) {
+        return (
+            R.length(objToArray(redFigures)) === 1 &&
+            R.length(objToArray(greenFigures)) === 2
+        );
+    } else {
+        return false;
+    }
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = figures => {
+    const isOrange = fig => R.equals(fig, 'orange');
+    const isAllOrange = R.filter(isOrange, figures);
+
+    return R.length(objToArray(isAllOrange)) === 4;
+};
 
 // 8. Не красная и не белая звезда.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = figures => {
+    const isInValidStar = R.anyPass([
+        R.propEq('star', 'red'),
+        R.propEq('star', 'white'),
+    ]);
+
+    return !isInValidStar(figures);
+};
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = figures => {
+    const isGreen = fig => R.equals(fig, 'green');
+    const isAllGreen = R.filter(isGreen, figures);
+
+    return R.length(objToArray(isAllGreen)) === 4;
+};
 
 // 10. Треугольник и квадрат одного цвета (не белого)
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = figures => {
+    const triangleColor = R.prop('triangle');
+    const referenceColor = triangleColor(figures);
+
+    if (referenceColor !== 'white') {
+        const checkRule = R.allPass([
+            R.propEq('square', referenceColor),
+            R.propEq('triangle', referenceColor),
+        ]);
+        return checkRule(figures);
+    } else {
+        return false;
+    }
+};
